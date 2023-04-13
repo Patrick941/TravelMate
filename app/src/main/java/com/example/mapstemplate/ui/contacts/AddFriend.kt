780055659376ee7c39e40eaa7470013fbad5f47e
Patrick941 <farmerp@tcd.ie>
2023-04-13 18:09:29 +0100

    "Fixed contacts page to display other using, fixed add friend page to stop crashing and beginning to add a method of adding users to a following list"

--- app/src/main/java/com/example/mapstemplate/ui/contacts/AddFriend.kt
+++ app/src/main/java/com/example/mapstemplate/ui/contacts/AddFriend.kt
@@ -6,9 +6,14 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mapstemplate.ContactsAdapter
 import com.example.mapstemplate.MainActivity
 import com.example.mapstemplate.R
+import com.example.mapstemplate.User
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class AddFriend : AppCompatActivity() {
 
@@ -17,29 +22,88 @@ class AddFriend : AppCompatActivity() {
 
     var tempString : String? = null
 
+    private val thisName = "AddFriend"
+
+    private lateinit var mDbRef: DatabaseReference
+    private lateinit var mAuth : FirebaseAuth
+
+    private lateinit var friendsList : ArrayList<User>
+    private lateinit var friendsNames : ArrayList<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.i("MyTag", "Program Created")
         setContentView(R.layout.activity_add_friend)
 
-        addButton = findViewById(R.id.SignUpButton)
-        email = findViewById(R.id.MessageText)
+        friendsList = ArrayList()
+        friendsNames = ArrayList()
 
-        addButton.setOnClickListener{
-            Log.i("FriendsTag", "Adding new friend with email ${email.text}")
-            tempString = email.text as String?
-            //val intent = Intent(this, MainActivity::class.java)
-            //intent.putExtra("email", email.text)
-            //val contactsFragment = ContactsFragment()
-            //val fragmentTransaction = fragmentManager.beginTransaction()
-            //val fragmentManager = supportFragmentManager
-            //fragmentTransaction.replace(R.id.nav_home, contactsFragment)
-            //fragmentTransaction.commit()
-            //startActivity(intent)
-            finish()
+        addButton = findViewById(R.id.addButton)
+        email = findViewById(R.id.editText)
+
+        private lateinit var contactsRecycler : RecyclerView
+        private lateinit var contactsAdapter : ContactsAdapter
+
+        email.setOnFocusChangeListener { _, hasFocus ->
+            addButton.visibility = if (hasFocus) Button.VISIBLE else Button.GONE
         }
 
+        mAuth = FirebaseAuth.getInstance()
 
+        mDbRef = FirebaseDatabase.getInstance().reference
+
+        mDbRef.child("user").addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //friendsList.clear()
+                for(postSnapshot in snapshot.children){
+                    val currentUser = postSnapshot.getValue(User::class.java)
+
+                    if(mAuth.currentUser?.uid == currentUser?.uid) {
+                        currentUser?.nick = "you"
+                    }
+                    friendsList.add(currentUser!!)
+                    currentUser.nick?.let { friendsNames.add(it) }
+                    Log.i("MyTag", "Adding user with email ${currentUser.email} to contacts")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+
+        contactsAdapter = ContactsAdapter(friendsNames)
+        contactsRecycler = root.findViewById(R.id.contactsRecycler)
+        contactsRecycler.layoutManager = LinearLayoutManager(context)
+        contactsRecycler.adapter = contactsAdapter
+
+        addButton.setOnClickListener{
+            //Log.i("FriendsTag", "Adding new friend with email ${email.text}")
+
+            finish()
+        }
     }
 
+    //logging activity changes
+    override fun onPause(){
+        super.onPause()
+        Log.i("MyTag", "pausing $thisName")
+    }
+    override fun onResume(){
+        super.onResume()
+        Log.i("MyTag", "resuming $thisName")
+    }
+    override fun onStart(){
+        super.onStart()
+        Log.i("MyTag", "starting $thisName")
+    }
+    override fun onStop(){
+        super.onStop()
+        Log.i("MyTag", "stopping $thisName")
+    }
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.i("MyTag", "destroying $thisName")
+    }
 }
