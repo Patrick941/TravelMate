a010ba2ab2b5c5a6799e7092187041f8b967433e
Benoit Gaudet <bgaudet38@gmail.com>
2023-04-13 12:36:28 +0100

    Store add itinerary image

--- app/src/main/java/com/example/mapstemplate/ui/add_itinerary/AddItineraryFragment.kt
+++ app/src/main/java/com/example/mapstemplate/ui/add_itinerary/AddItineraryFragment.kt
@@ -1,7 +1,9 @@
 package com.example.mapstemplate.ui.add_itinerary
 
+import android.app.Activity
 import android.content.Intent
 import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -12,6 +14,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.mapstemplate.HomeActivity
 import com.example.mapstemplate.R
 import com.example.mapstemplate.activities.ItineraryActivity
@@ -19,14 +22,34 @@ import com.example.travelapp.itineraries.Itinerary
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import java.io.File
+import java.util.*
 
 
 class AddItineraryFragment : Fragment() {
     lateinit var addButton: Button
     lateinit var editTextTitle: EditText
+    lateinit var searchImageButton: Button
+    lateinit var imageView: ImageView
 
+    private val storage = FirebaseStorage.getInstance()
+    private val storageRef = storage.reference
     private val mAuth = FirebaseAuth.getInstance()
     private val db = Firebase.firestore
+
+    private var uriImage: Uri? = null
+
+    // Receiver
+    private val getResult =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+            if (it.resultCode == Activity.RESULT_OK && it.data != null) {
+                uriImage = it.data!!.data!!
+                imageView.setImageURI(uriImage)
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,11 +64,31 @@ class AddItineraryFragment : Fragment() {
 
         addButton = view.findViewById(R.id.button_add_itinerary)
         editTextTitle = view.findViewById(R.id.editText_add_itinerary_title)
+        searchImageButton = view.findViewById(R.id.button_add_itinerary_image)
+        imageView = view.findViewById(R.id.add_itinerary_imageView)
 
         setupAddButton()
+        setupAddImageButton()
 
         // Inflate the layout for this fragment
         return binding
+    }
+
+    fun setupAddImageButton() {
+        searchImageButton.setOnClickListener {
+            selectImageFromPhone()
+        }
+    }
+
+    fun selectImageFromPhone() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        try {
+            getResult.launch(intent)
+        } catch (e: Exception) {
+            Toast.makeText(context, "Problem to load storage images", Toast.LENGTH_SHORT).show()
+        }
     }
 
     /**
@@ -55,13 +98,29 @@ class AddItineraryFragment : Fragment() {
         addButton.setOnClickListener {
             if (editTextTitle.text.toString().equals("")) {
                 Toast.makeText(context, "You need to enter an itinerary name", Toast.LENGTH_SHORT).show()
-                editTextTitle.setHintTextColor(Color.RED)
             } else {
-                editTextTitle.setHintTextColor(Color.BLACK)
                 addItinerary(editTextTitle.text.toString())
                 editTextTitle.setText("")
             }
         }
+    }
+
+    /**
+     * Save a new image in cloud storage if the user has selected an image
+     */
+    fun saveImageInFirebase(itineraryId: String,) {
+        if (uriImage == null)
+            return
+
+        val imageRef = storageRef.root.child("images_itineraries/${mAuth.currentUser!!.uid}/${itineraryId}/main_image" )
+
+        imageRef.putFile(uriImage!!)
+            .addOnSuccessListener{
+                Log.d("DEBUG", "Save image")
+            }
+            .addOnFailureListener{
+                Toast.makeText(context, "Upload image failed...", Toast.LENGTH_SHORT).show()
+            }
     }
 
     /**
@@ -77,6 +136,10 @@ class AddItineraryFragment : Fragment() {
         db.collection("itineraries")
             .add(itineraryHashMap)
             .addOnSuccessListener { documentReference ->
+                Log.d("DEBUG", "Document added")
+                // Save the selected image
+                saveImageInFirebase(documentReference.id)
+
                 // Add the itinerary in the currentUserItineraryList
                 val itinerary = Itinerary(title, documentReference.id)
                 HomeActivity.currentUserItineraryList.add(itinerary)
