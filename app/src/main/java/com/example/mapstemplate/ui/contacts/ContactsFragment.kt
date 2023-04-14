85dd05330da20abdf43266e52e744bb3ae83f439
Patrick941 <farmerp@tcd.ie>
2023-04-14 04:29:56 +0100

    "Beginning likes pages, displaying two lists, one for likes one for feed. Not yet working"

--- app/src/main/java/com/example/mapstemplate/ui/contacts/ContactsFragment.kt
+++ app/src/main/java/com/example/mapstemplate/ui/contacts/ContactsFragment.kt
@@ -1,5 +1,110 @@
-
 package com.example.mapstemplate.ui.contacts
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import com.example.mapstemplate.HomeActivity
+import com.example.mapstemplate.R
+import com.example.mapstemplate.activities.ItineraryActivity
+import com.example.mapstemplate.databinding.FragmentSlideshowBinding
+import com.example.travelapp.adapters.ItineraryListAdapter
+import com.example.travelapp.itineraries.Itinerary
+
+class ContactsFragment : Fragment() {
+    private var _binding: FragmentSlideshowBinding? = null
+    private val binding get() = _binding!!
+
+    lateinit var listViewItinerary: ListView
+    lateinit var listViewItineraryFollowing: ListView
+    lateinit var itineraryListAdapter: ItineraryListAdapter
+    lateinit var itineraryListAdapterFollowing: ItineraryListAdapter
+
+    val itineraryList: ArrayList<Itinerary> = ArrayList()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        itineraryList.addAll(HomeActivity.globalItineraryList)
+
+        listViewItinerary = root.findViewById(R.id.likedItineraries)
+        listViewItineraryFollowing = root.findViewById(R.id.followingItineraries)
+
+        setupItineraryListView()
+        setupItineraryListViewFollowing()
+
+        val searchButton = root.findViewById<Button>(R.id.btnSearch)
+        val inputField = root.findViewById<EditText>(R.id.itineraryName)
+        var enteredSearch = ""
+
+        /*searchButton.setOnClickListener {
+            val newItineraryList = ArrayList<Itinerary>()
+            enteredSearch = inputField.text.toString()
+            for (i in HomeActivity.globalItineraryList) {
+                if (i.name.contains(enteredSearch, true)) {
+                    newItineraryList.add(i)
+                } else {
+                    Toast.makeText(
+                        context,
+                        "Please, enter an itinerary name",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+            itineraryList.clear()
+            itineraryList.addAll(newItineraryList)
+            itineraryListAdapter.notifyDataSetChanged()
+
+        }*/
+        return root
+    }
+
+    fun setupItineraryListView() {
+        itineraryListAdapter = ItineraryListAdapter(requireContext(), itineraryList) { position ->
+            val intent = Intent(context, ItineraryActivity::class.java)
+            intent.putExtra("itinerary_index", position)
+            intent.putExtra("is_global", true)
+            startActivity(intent)
+        }
+
+        listViewItinerary.isClickable = true
+        listViewItinerary.adapter = itineraryListAdapter
+    }
+
+    fun setupItineraryListViewFollowing() {
+        itineraryListAdapterFollowing = ItineraryListAdapter(requireContext(), itineraryList) { position ->
+            val intent = Intent(context, ItineraryActivity::class.java)
+            intent.putExtra("itinerary_index", position)
+            intent.putExtra("is_global", true)
+            startActivity(intent)
+        }
+
+        listViewItineraryFollowing.isClickable = true
+        listViewItineraryFollowing.adapter = itineraryListAdapter
+    }
+
+
+
+
+
+    override fun onResume() {
+        super.onResume()
+        // update the data in the listView
+        itineraryListAdapter.notifyDataSetChanged()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
+/*package com.example.mapstemplate.ui.contacts
 
 import android.content.Intent
 import android.os.Bundle
@@ -162,4 +267,4 @@ class ContactsFragment : Fragment() {
         Log.i("MyTag", "destroying view for $thisName")
         _binding = null
     }
-}
+}*/
